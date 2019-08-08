fb7309f6f2c35b596a9123875ba76b98bc87d331
Dekevion Wade <Dekevionwade@yahoo.com>
2019-08-08 11:26:02 -0500

    answers

---
+++ index.js
@@ -0,0 +1,19 @@
+// exercise 1
+for ( var i = 0; i < 25; i= i + 1) {
+    console.log("*");
+}
+
+// exercise 2
+
+for (var c = 0; c <= 5; c= c + 1) {
+    if (c === 2) {
+        console.log('two');
+    } else if ( c === 4) {
+        console.log("four")
+    } else {
+        console.log (c);
+    }
+}
+
+
+
